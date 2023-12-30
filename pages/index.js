aa03d5784992c156fbcd0493882b177e13ddc6b4
smartdriver321 <tinggaldidisneyland@gmail.com>
2023-12-30 19:23:20 +0700

    Video-07-Create-Product-Details-Page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Layout from '../components/Layout';
+import Layout from '@/components/Layout';
 import ProductItem from '@/components/ProductItem';
-import data from '../utils/data';
+import data from '@/utils/data';
 
 export default function Home() {
   return (
