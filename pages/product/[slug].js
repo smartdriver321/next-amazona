97a16b808af5b8bb442b3580089d3e814f6df206
smartdriver321 <tinggaldidisneyland@gmail.com>
2023-12-31 10:31:02 +0700

    Video-09-Create-Cart-Page

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -8,6 +8,7 @@ import React, { useContext } from 'react';
 
 export default function ProductScreen() {
   const { state, dispatch } = useContext(Store);
+  const router = useRouter();
   const { query } = useRouter();
   const { slug } = query;
   const product = data.products.find((x) => x.slug === slug);
@@ -25,6 +26,7 @@ export default function ProductScreen() {
       return;
     }
     dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
+    router.push('/cart');
   };
 
   return (
