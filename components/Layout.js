bf8e33e740079e5cd37994e1ff96f5c3fec72c55
smartdriver321 <tinggaldidisneyland@gmail.com>
2023-12-31 19:25:01 +0700

    Video-14-Create-Login-API

--- components/Layout.js
+++ components/Layout.js
@@ -1,17 +1,20 @@
-import { Store } from '@/utils/Store';
+import { useSession } from 'next-auth/react';
 import Head from 'next/head';
 import Link from 'next/link';
 import React, { useContext, useEffect, useState } from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Store } from '@/utils/Store';
 
 export default function Layout({ title, children }) {
+  const { status, data: session } = useSession();
+
   const { state } = useContext(Store);
   const { cart } = state;
   const [cartItemsCount, setCartItemsCount] = useState(0);
-
   useEffect(() => {
     setCartItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0));
   }, [cart.cartItems]);
-
   return (
     <>
       <Head>
@@ -20,14 +23,16 @@ export default function Layout({ title, children }) {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <div className='flex min-h-screen flex-col justify-between'>
+      <ToastContainer position='bottom-center' limit={1} />
+
+      <div className='flex min-h-screen flex-col justify-between '>
         <header>
           <nav className='flex h-12 items-center px-4 justify-between shadow-md'>
-            <Link className='text-lg font-bold' href='/'>
+            <Link href='/' className='text-lg font-bold'>
               amazona
             </Link>
             <div>
-              <Link className='p-2' href='/cart'>
+              <Link href='/cart' className='p-2'>
                 Cart
                 {cartItemsCount > 0 && (
                   <span className='ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white'>
@@ -35,15 +40,22 @@ export default function Layout({ title, children }) {
                   </span>
                 )}
               </Link>
-              <Link className='p-2' href='/login'>
-                Login
-              </Link>
+
+              {status === 'loading' ? (
+                'Loading'
+              ) : session?.user ? (
+                session.user.name
+              ) : (
+                <Link href='/login' className='p-2'>
+                  Login
+                </Link>
+              )}
             </div>
           </nav>
         </header>
         <main className='container m-auto mt-4 px-4'>{children}</main>
         <footer className='flex h-10 justify-center items-center shadow-inner'>
-          Copyright @ 2022 Amazona
+          <p>Copyright © 2022 Amazona</p>
         </footer>
       </div>
     </>
--- pages/login.js
+++ pages/login.js
@@ -1,19 +1,43 @@
-import Layout from '@/components/Layout';
 import Link from 'next/link';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { signIn, useSession } from 'next-auth/react';
 import { useForm } from 'react-hook-form';
+import Layout from '@/components/Layout';
+import { getError } from '@/utils/error';
+import { toast } from 'react-toastify';
+import { useRouter } from 'next/router';
 
-export default function loginScreen() {
+export default function LoginScreen() {
+  const { data: session } = useSession();
+
+  const router = useRouter();
+  const { redirect } = router.query;
+
+  useEffect(() => {
+    if (session?.user) {
+      router.push(redirect || '/');
+    }
+  }, [router, session, redirect]);
+
   const {
     handleSubmit,
     register,
     formState: { errors },
   } = useForm();
-
-  const submitHandler = ({ email, password }) => {
-    console.log(email, password);
+  const submitHandler = async ({ email, password }) => {
+    try {
+      const result = await signIn('credentials', {
+        redirect: false,
+        email,
+        password,
+      });
+      if (result.error) {
+        toast.error(result.error);
+      }
+    } catch (err) {
+      toast.error(getError(err));
+    }
   };
-
   return (
     <Layout title='Login'>
       <form
@@ -26,10 +50,10 @@ export default function loginScreen() {
           <input
             type='email'
             {...register('email', {
-              required: 'please enter email',
+              required: 'Please enter email',
               pattern: {
                 value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
-                message: 'please enter valid email',
+                message: 'Please enter valid email',
               },
             })}
             className='w-full'
@@ -56,10 +80,10 @@ export default function loginScreen() {
             <div className='text-red-500 '>{errors.password.message}</div>
           )}
         </div>
-        <div className='mb-4'>
+        <div className='mb-4 '>
           <button className='primary-button'>Login</button>
         </div>
-        <div className='mb-4'>
+        <div className='mb-4 '>
           Don&apos;t have an account? &nbsp;
           <Link href='register'>Register</Link>
         </div>
