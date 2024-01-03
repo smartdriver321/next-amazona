b5c6e0bc75a5536e1bc26d92117788b21f9c6192
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-01-03 15:19:49 +0700

    Video-22-Create-Register-Screen

--- pages/register.js
+++ pages/register.js
@@ -1,11 +1,12 @@
-import Link from 'next/link';
 import React, { useEffect } from 'react';
+import axios from 'axios';
 import { signIn, useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 import { useForm } from 'react-hook-form';
-import Layout from '@/components/Layout';
 import { getError } from '@/utils/error';
 import { toast } from 'react-toastify';
-import { useRouter } from 'next/router';
+import Layout from '@/components/Layout';
+import Link from 'next/link';
 
 export default function LoginScreen() {
   const { data: session } = useSession();
@@ -21,11 +22,18 @@ export default function LoginScreen() {
   const {
     handleSubmit,
     register,
+    getValues,
     formState: { errors },
   } = useForm();
 
-  const submitHandler = async ({ email, password }) => {
+  const submitHandler = async ({ name, email, password }) => {
     try {
+      await axios.post('/api/auth/signup', {
+        name,
+        email,
+        password,
+      });
+
       const result = await signIn('credentials', {
         redirect: false,
         email,
@@ -38,13 +46,30 @@ export default function LoginScreen() {
       toast.error(getError(err));
     }
   };
+
   return (
-    <Layout title='Login'>
+    <Layout title='Create Account'>
       <form
         className='mx-auto max-w-screen-md'
         onSubmit={handleSubmit(submitHandler)}
       >
-        <h1 className='mb-4 text-xl'>Login</h1>
+        <h1 className='mb-4 text-xl'>Create Account</h1>
+        <div className='mb-4'>
+          <label htmlFor='name'>Name</label>
+          <input
+            type='text'
+            className='w-full'
+            id='name'
+            autoFocus
+            {...register('name', {
+              required: 'Please enter name',
+            })}
+          />
+          {errors.name && (
+            <div className='text-red-500'>{errors.name.message}</div>
+          )}
+        </div>
+
         <div className='mb-4'>
           <label htmlFor='email'>Email</label>
           <input
@@ -58,12 +83,12 @@ export default function LoginScreen() {
             })}
             className='w-full'
             id='email'
-            autoFocus
           ></input>
           {errors.email && (
             <div className='text-red-500'>{errors.email.message}</div>
           )}
         </div>
+
         <div className='mb-4'>
           <label htmlFor='password'>Password</label>
           <input
@@ -80,9 +105,37 @@ export default function LoginScreen() {
             <div className='text-red-500 '>{errors.password.message}</div>
           )}
         </div>
-        <div className='mb-4 '>
-          <button className='primary-button'>Login</button>
+
+        <div className='mb-4'>
+          <label htmlFor='confirmPassword'>Confirm Password</label>
+          <input
+            className='w-full'
+            type='password'
+            id='confirmPassword'
+            {...register('confirmPassword', {
+              required: 'Please enter confirm password',
+              validate: (value) => value === getValues('password'),
+              minLength: {
+                value: 6,
+                message: 'confirm password is more than 5 chars',
+              },
+            })}
+          />
+          {errors.confirmPassword && (
+            <div className='text-red-500 '>
+              {errors.confirmPassword.message}
+            </div>
+          )}
+          {errors.confirmPassword &&
+            errors.confirmPassword.type === 'validate' && (
+              <div className='text-red-500 '>Password do not match</div>
+            )}
         </div>
+
+        <div className='mb-4'>
+          <button className='primary-button'>Register</button>
+        </div>
+
         <div className='mb-4 '>
           Don&apos;t have an account? &nbsp;
           <Link href={`/register?redirect=${redirect || '/'}`}>Register</Link>
